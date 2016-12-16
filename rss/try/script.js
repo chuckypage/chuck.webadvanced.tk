bfcfbf476c9dd92983457360b10375ea0b55add4
Chuck Page <pagec892@newschool.edu>
2016-12-16 03:07:37 +0000

    message

--- rss/try/script.js
+++ rss/try/script.js
@@ -2,6 +2,7 @@
    
 // First RSS feed
 
+
     var myString = "";
     
       $(function () {
@@ -35,11 +36,52 @@
         
 // var msg = new SpeechSynthesisUtterance('fuck this');
 //     	window.speechSynthesis.speak(msg);
-        
-        
+   
+    function slideSwitch() {
+        var $active = $('div#slideshow IMG.active');
+        var $next = $active.next();    
+
+        $next.addClass('active');
+
+        $active.removeClass('active');
+    }
+
+    $(function() {
+        setInterval( "slideSwitch()", 5000 );
+    });
+
 
  
       });
+      $(function () {
+    
+    /* SET PARAMETERS */
+    var change_img_time 	= 5000;	
+    var transition_speed	= 300;
+    
+    var simple_slideshow	= $("#exampleSlider"),
+        listItems 			= simple_slideshow.children('li'),
+        listLen				= listItems.length,
+        i 					= 0,
+		
+        changeList = function () {
+		
+			listItems.eq(i).fadeOut(transition_speed, function () {
+				i += 1;
+				if (i === listLen) {
+					i = 0;
+				}
+				listItems.eq(i).fadeIn(transition_speed);
+			});
+
+        };
+		
+    listItems.not(':first').hide();
+    setInterval(changeList, change_img_time);
+	
+});
+
+      
      
 
 
