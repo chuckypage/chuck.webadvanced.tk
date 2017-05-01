1690cb71a8645a93ffdd83c65093743d084240c4
Chuck Page <pagec892@newschool.edu>
2017-05-01 23:14:18 +0000

    message

---
+++ Thesis/pornrss/script.js
@@ -0,0 +1,70 @@
+//  // var imgcounter = 0;
+//  // var myString = "";
+     
+
+   
+    
+//  //      $(function () {
+//  //        //This overwrites whatever is in #content, so we should find another container for our second feed..
+//  //        //going to pull from Craigslist, as an example
+//  //        var statement = "select * from feed where url='https://www.porndig.com/rss/channel/815/secretary.xml'";
+//  //        $.queryYQL(statement, "json", undefined, function (data) {
+//  //          // do something with "data".
+//  //          console.log(data);
+//  //          for (var i = 0; i < data.query.results.item.length; i++) {
+//  //            console.log(data.query.results.item[i].title[0]);
+            
+//  //            $("#porn-titles").append("<p>" + data.query.results.item[i].title[0] + "</p>");
+            
+            
+            
+
+//  //            var img;
+//  //            if (data.query.results.item[i].enclosure) {
+//  //              console.log(img);
+//  //              img = data.query.results.item[i].enclosure.resource;
+//  //            }
+
+//  //            console.log (img);
+//  //            $("#porn-images").append("<img src='" + img + "' id='img-" + imgcounter + "'>");
+//  //            imgcounter++;
+            
+//  //          }
+// // // First RSS feed
+
+// //     var myString = "";
+    
+// //       $(function () {
+// //         var statement = "select * from feed where url='https://www.porndig.com/rss/channel/815/secretary.xml'";
+// //         $.queryYQL(statement, "json", undefined, function (data) {
+// //           // do something with "data".
+// //           console.log(data.query.results.item);
+// //           // console.log(data)
+// //           for (var i = 0; i < data.query.results.item.length; i++) {
+// //             console.log(data.query.results.item[i].title[0]);
+// //             //$("#tributes").append("<p>" + data.query.results.item[i].title + "</p>");
+// //             var toRead = data.query.results.item[i].title;
+// //             myString = myString + data.query.results.item[i].title + ".   ";
+           
+           
+
+            
+// //           }
+
+
+//     //       $(function () {
+//     // var t = $('#content').empty();
+//     // var statement = "select * from feed where url='https://www.porndig.com/rss/channel/815/secretary.xml'";
+//     // $.queryYQL(statement, "json", undefined, function (data) {
+//     //   // do something with "data".
+
+//     //   console.log(data.query.results.item);
+      
+//     //   for (var i = 0; i < data.query.results.item.length; i++) {
+//     //     console.log(data.query.results.item[i].title[0]);
+//     //     $("body").append("<p>" + data.query.results.item[i].title[0] + "</p>")
+        
+//     //   }
+      
+//     });
+// });
